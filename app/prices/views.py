6c22f3e2be0b180f82fd43b5f7b2de2898dded0e
hsw0905 <hsw0905@gmail.com>
2020-09-17 22:44:30 +0900

    HotFix: modify CarSerializer from ios request

--- app/prices/views.py
+++ app/prices/views.py
@@ -1,24 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
-from requests import Response
-from rest_framework import mixins
-from rest_framework.permissions import IsAuthenticated
-from rest_framework.viewsets import GenericViewSet
-
-from prices.models import CarPrice
-from prices.serializers import CarPriceSerializer
-
-
-class CarPriceViewSet(mixins.ListModelMixin, GenericViewSet):
-    queryset = CarPrice.objects.all()
-    serializer_class = CarPriceSerializer
-    permission_classes = [IsAuthenticated, ]
-
-    def get_queryset(self):
-        # ios 요청 : /carzones/123/cars/4/prices
-        queryset = super().get_queryset()
-        queryset = queryset.filter(car_id=self.kwargs.get('car_pk'))
-        return queryset
 
 
