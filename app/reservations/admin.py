39dee488a0618d01a2d5cd15ce3190fbc61e00ce
hsw0905 <hsw0905@gmail.com>
2020-09-28 15:22:00 +0900

    Feat: reservations, payments model, not function yet

--- app/reservations/admin.py
+++ app/reservations/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
 
 # Register your models here.
+from import_export.admin import ImportExportMixin
+
+from reservations.models import Reservation, ReservationStatus
+
+
+@admin.register(Reservation)
+class ReservationAdmin(ImportExportMixin, admin.ModelAdmin):
+    list_display = ('id',
+                    'member',
+                    'zone',
+                    'car',
+                    'insurance',
+                    'date_time_start',
+                    'date_time_end',
+                    'date_time_extension',
+                    'created_at',
+                    'updated_at')
+
+
+@admin.register(ReservationStatus)
+class ReservationStatusAdmin(ImportExportMixin, admin.ModelAdmin):
+    list_display = ('id',
+                    'reservation',
+                    'status',
+                    'created_at',
+                    'updated_at')
--- app/reservations/models.py
+++ app/reservations/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils.translation import gettext as _
 
+
 # Create your models here.
 
 
@@ -10,21 +11,30 @@ class Reservation(models.Model):
         STANDARD = 'standard', _('스탠다드')
         LIGHT = 'light', _('라이트')
 
+    member = models.ForeignKey('members.Member', on_delete=models.CASCADE, related_name='reservation_members')
+    zone = models.ForeignKey('carzones.CarZone', on_delete=models.CASCADE, related_name='reservation_zones')
+    car = models.ForeignKey('cars.Car', on_delete=models.CASCADE, related_name='reservation_cars')
+    insurance = models.CharField(choices=ChoiceInsuranceType.choices, default=ChoiceInsuranceType.LIGHT,
+                                 help_text='차량손해면책상품', max_length=30)
+    date_time_start = models.DateTimeField(help_text='예약시작날짜시간')
+    date_time_end = models.DateTimeField(help_text='예약종료날짜시간')
+    date_time_extension = models.DateTimeField(help_text='연장종료날짜시간')
+    created_at = models.DateTimeField(auto_now_add=True, help_text='TimeStamp')
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class ReservationStatus(models.Model):
     class ChoiceStatus(models.TextChoices):
         NOTPAID = 'not_paid', _('결제전')
-        PAID = 'paid', _('결제완료')
+        PAID_1 = 'paid_1', _('운행전결제완료')
+        PAID_2 = 'paid_2', _('운행후결제완료')
         USING = 'using', _('사용중')
-        EXTENDED = 'extended', _('추가연장')
+        EXTENDED = 'extended', _('사용중추가연장')
         FINISHED = 'finished', _('반납완료')
 
-    member = models.ForeignKey('members.Member', on_delete=models.CASCADE, related_name='reservations')
-    zone = models.ForeignKey('carzones.CarZone', on_delete=models.CASCADE, related_name='reservations')
-    car = models.ForeignKey('cars.Car', on_delete=models.CASCADE, related_name='reservations')
-    insurance = models.CharField(choices=ChoiceInsuranceType.choices.label, default=ChoiceInsuranceType.LIGHT,
-                                 help_text='차량손해면책상품')
-    date_start = models.DateTimeField(help_text='예약시작날짜시간')
-    date_end = models.DateTimeField(help_text='예약종료날짜시간')
-    date_extension = models.DateTimeField(default=date_end, help_text='연장종료날짜시간')
-    status = models.CharField(choices=ChoiceStatus.choices.label, default=ChoiceStatus.NOTPAID, help_text='대여진행상태')
+    reservation = models.OneToOneField('reservations.Reservation', on_delete=models.CASCADE,
+                                       related_name='status')
+    status = models.CharField(choices=ChoiceStatus.choices, default=ChoiceStatus.NOTPAID, max_length=30,
+                              help_text='대여진행상태')
     created_at = models.DateTimeField(auto_now_add=True, help_text='TimeStamp')
     updated_at = models.DateTimeField(auto_now=True)
